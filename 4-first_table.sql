217b8d61cee3b6dfef7c71f69730a61f1585f933
Nafypy <Nafypy9@gmail.com>
2024-06-06 06:21:22 -0700

    revisit  table

---
+++ 4-first_table.sql
@@ -0,0 +1,7 @@
+--create table called first_table;
+-- the first_table should have( id of INT and (name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+	VALUES
+	ID INT,
+	NAME VARCHAR(256)
+	);
