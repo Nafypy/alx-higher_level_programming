5da71f4e67fdb29ccd87ef2f347831f252d2e3c1
Nafypy <Nafypy9@gmail.com>
2024-07-18 00:13:49 -0700

    great task

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 --create a table named first_table
 --first_table should have id INT  and name(VARCHAR(256))
-CREATE TABLE IF NOT EXISTS first_table (id INT name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
