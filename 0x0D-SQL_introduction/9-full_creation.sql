5b1df1bf601fa3d8628016a9359be2a26da5b766
Nafypy <Nafypy9@gmail.com>
2024-07-19 08:27:44 -0700

    full_correction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- create a table with multiple rows
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
---insert records into this table
+-- insert records into this table
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Alex", 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
