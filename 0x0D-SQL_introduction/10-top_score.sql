d1ab860c6d6934d04ea7e812924100de9236862d
Nafypy <Nafypy9@gmail.com>
2024-07-19 08:24:40 -0700

    list all records

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
---Write a script that list all records of a table second_table
+-- Write a script that list all records of a table second_table
 SELECT score, name FROM second_table ORDER BY score DESC;
