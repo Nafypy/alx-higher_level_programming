37bb12208fc519a4267ead1f4b4de2cae93769c4
Nafypy <nafypy9@gmail.com>
2024-07-25 14:47:49 +0000

    9 cities

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
+USE hbtn_0d_usa;
 -- Write a script that lists all cities contained in the database hbtn_0d_usa
 
 SELECT cities.id AS cities_id, cities.name AS cities_name, states.nane AS states-name FROM cities
