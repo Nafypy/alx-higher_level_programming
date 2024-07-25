5ee1df802725d4eec11fb8ba5e174c84c74a91af
Nafypy <nafypy9@gmail.com>
2024-07-25 14:44:47 +0000

    states and cities

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Write a script that lists all cities contained in the database hbtn_0d_usa
+
+SELECT cities.id AS cities_id, cities.name AS cities_name, states.nane AS states-name FROM cities
+JOIN states ON cities.states_id = states.id
+ORDER BY cities.id;
