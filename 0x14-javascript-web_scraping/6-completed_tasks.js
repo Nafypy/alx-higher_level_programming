8f22c743e2b10e51162aee25f2ddfeef5a4a9dcb
Nafypy <nafypy9@gmail.com>
2024-07-31 10:00:59 -0700

    Compute

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,39 @@
+#!/urs/bin/bash
+/* Write a script that computes the number of tasks completed by user id */
+
+const request = require('request');
+const process = require('process');
+
+if (process.argv.length !== 3) {
+	console.error('Usage: ./compute_tasks.js API_URL');
+} else {
+	const apiUrl = process.argv[2];
+	request.get(apiUrl, (error, response, body) => {
+		if (error) {
+			console.error('Error:', error);
+			return;
+		}
+
+		try {
+			const todos = JSON.parse(body);
+			const userTasks = {};
+
+			todos.forEach(todo => {
+				if (todo.completed) {
+					if (!userTasks[todo.userId]) {
+						userTasks[todo.userId] = 0;
+					}
+					userTasks[todo.userId]++;
+				}
+			});
+			
+			for (const userId in userTasks) {
+				console.log(`User ${userId}: ${userTasks[userId]} completed tasks`);
+				}
+		} catch (e) {
+			console.error('Error parsing JSON:', e);
+		}
+	});
+}
+
+
