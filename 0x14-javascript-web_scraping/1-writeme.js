fde2a293b80750e845693d8f1b2a3cc60eee8c11
Nafypy <nafypy9@gmail.com>
2024-07-31 02:07:47 -0700

    write.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
- * A script that write a file to a string
+ * A script that write a string to a file
  */
 const fs = require('fs');
 const process = require('process');
@@ -16,7 +16,7 @@ function writeFile(filePath, content) {
 	});
 }
 if(process.argv.length !== 4) {
-	console.error('Usage: ./write_file.js FILE_PATH STRING_TO_PATH');
+	console.error('Usage: ./write_file.js FILE_PATH STRING_TO_WRITE');
 }else {
 	const filePath = process.argv[2];
 	const content = process.argv[3];
